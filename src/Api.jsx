8ed3fc88b0a9af1eab56baf910c769cc78b53004
Ahnjaewon <vutus5@naver.com>
2023-10-06 15:09:13 +0900

    update

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// axios.defaults.baseURL =
+//     process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://34.66.151.72:8080';
+
 axios.defaults.baseURL =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://34.66.151.72:8080';
+    process.env.NODE_ENV === 'development' && 'http://localhost:8080';
 
 
 export const apiNoToken = async (url, method, data) => {
