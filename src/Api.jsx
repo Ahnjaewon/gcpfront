22efe2f803a8618f4cae8eb5f3d94c6a2c18b287
Ahnjaewon <vutus5@naver.com>
2023-10-06 11:22:59 +0900

    update

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://34.66.151.72:8080/';
+axios.defaults.baseURL =
+    process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://34.66.151.72:8080';
+
 
 export const apiNoToken = async (url, method, data) => {
 
